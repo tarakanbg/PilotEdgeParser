0830d908804af943f6dc21727341b9def8ef0378
Svilen Vassilev <svilen@rubystudio.net>
2016-11-20 00:56:30 +0200

    Dates, layout

--- PilotEdgeParser/Form1.Designer.cs
+++ PilotEdgeParser/Form1.Designer.cs
@@ -28,58 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(521, 111);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 120);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.dataGridView1.Size = new System.Drawing.Size(521, 217);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 339);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.richTextBox1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.Name = "Form1";
-            this.Text = "Pilotedge Active Controllers";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+      this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+      this.dataGridViewATC = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridViewATC)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // richTextBoxInfo
+      // 
+      this.richTextBoxInfo.Location = new System.Drawing.Point(13, 0);
+      this.richTextBoxInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.richTextBoxInfo.Name = "richTextBoxInfo";
+      this.richTextBoxInfo.ReadOnly = true;
+      this.richTextBoxInfo.Size = new System.Drawing.Size(536, 66);
+      this.richTextBoxInfo.TabIndex = 0;
+      this.richTextBoxInfo.Text = "";
+      this.richTextBoxInfo.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+      // 
+      // dataGridViewATC
+      // 
+      this.dataGridViewATC.AllowUserToAddRows = false;
+      this.dataGridViewATC.AllowUserToDeleteRows = false;
+      this.dataGridViewATC.AllowUserToOrderColumns = true;
+      this.dataGridViewATC.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+      this.dataGridViewATC.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+      this.dataGridViewATC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridViewATC.Location = new System.Drawing.Point(13, 73);
+      this.dataGridViewATC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.dataGridViewATC.Name = "dataGridViewATC";
+      this.dataGridViewATC.ReadOnly = true;
+      this.dataGridViewATC.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+      this.dataGridViewATC.Size = new System.Drawing.Size(536, 197);
+      this.dataGridViewATC.TabIndex = 1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.Control;
+      this.ClientSize = new System.Drawing.Size(562, 271);
+      this.Controls.Add(this.dataGridViewATC);
+      this.Controls.Add(this.richTextBoxInfo);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form1";
+      this.ShowIcon = false;
+      this.Text = "Pilotedge Active Controllers";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridViewATC)).EndInit();
+      this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.DataGridView dataGridViewATC;
     }
 }
 
--- PilotEdgeParser/Form1.cs
+++ PilotEdgeParser/Form1.cs
@@ -13,103 +13,105 @@ using System.IO;
 
 namespace PilotEdgeParser
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
+      WebClient Client = new WebClient();
+      string myTempFileOrig = Path.Combine(Path.GetTempPath(), "pe.txt");
+      string myTempFileParsed = Path.Combine(Path.GetTempPath(), "pe_parsed.txt");
+      string myTempFileInfo = Path.Combine(Path.GetTempPath(), "pe_info.txt");
+      Client.DownloadFile("http://map.pilotedge.net/vspro.dat", myTempFileOrig);
+
+      using (Stream fileStream = File.Open(myTempFileOrig, FileMode.Open))
+      using (StreamReader reader = new StreamReader(fileStream))
+      {
+        using (StreamWriter sw = new StreamWriter(myTempFileParsed))
         {
-            WebClient Client = new WebClient();
-            string myTempFileOrig = Path.Combine(Path.GetTempPath(), "pe.txt");
-            string myTempFileParsed = Path.Combine(Path.GetTempPath(), "pe_parsed.txt");
-            string myTempFileInfo = Path.Combine(Path.GetTempPath(), "pe_info.txt");
-            Client.DownloadFile("http://map.pilotedge.net/vspro.dat", myTempFileOrig);
-            // open the file in a way so that we can read it line by line
-            using (Stream fileStream = File.Open(myTempFileOrig, FileMode.Open))
-            using (StreamReader reader = new StreamReader(fileStream))
+
+          using (StreamWriter swinfo = new StreamWriter(myTempFileInfo))
+          {
+
+            string line = null;
+            int pilot_count = 0;
+            int drone_count = 0;
+            do
             {
-                using (StreamWriter sw = new StreamWriter(myTempFileParsed))
+              line = reader.ReadLine();
+              if (line == null)
+              {
+                // there are no more lines; break out of the loop
+                break;
+              }
+
+              string[] parts = line.Split(':');
+              // "CS" in parts[0] 
+              // "Name" in parts[2] 
+              // "ROLE" in parts[3] 
+
+              if (parts.Length > 2)
+              {
+                if (parts[3] == "ATC")
                 {
-
-                    using (StreamWriter swinfo = new StreamWriter(myTempFileInfo))
-                    {
-
-                        string line = null;
-                        int pilot_count = 0;
-                        int drone_count = 0;
-                        do
-                        {
-                            // get the next line from the file
-                            line = reader.ReadLine();
-                            if (line == null)
-                            {
-                                // there are no more lines; break out of the loop
-                                break;
-                            }
-
-                            // split the line on each semicolon character
-                            string[] parts = line.Split(':');
-                            // now the array contains values as such:
-                            // "CS" in parts[0] 
-                            // "Surname" in parts[1] 
-                            // "Name" in parts[2] 
-                            // "ROLE" in parts[3] 
-
-                            if (parts.Length > 2)
-                            {
-                                if (parts[3] == "ATC")
-                                {
-                                    string row_string = parts[0] + "," + parts[2];
-                                    sw.WriteLine(row_string);
-                                }
-
-                                else if (parts[3] == "PILOT")
-
-                                {
-                                    if (parts[2] == "DRONE")
-                                    {
-                                        drone_count += 1;
-                                    } else
-                                    {
-                                        pilot_count += 1;
-                                    }
-                                }
-                            }
-
-                        } while (true);
-
-                        swinfo.WriteLine($"Pilots on the network: {pilot_count}");
-                        swinfo.WriteLine($"Drones on the network: {drone_count}");
-                    }
-
+                  string year = parts[37].Substring(0, 4);
+                  string month = parts[37].Substring(4, 2);
+                  string day = parts[37].Substring(6, 2);
+                  string hour = parts[37].Substring(8, 2);
+                  string minute = parts[37].Substring(10, 2);
+                  string second = parts[37].Substring(12, 2);
+                  string con_string = $"{month}/{day}/{year} {hour}:{minute}:{second}";
+                  DateTime date_obj = Convert.ToDateTime(con_string);
+                  string date_str = date_obj.ToString("MM'/'dd'/'yyyy HH':'mm':'ss");
+                  string row_string = parts[0] + "," + parts[2] + "," + date_str;
+                  sw.WriteLine(row_string);
                 }
-               
-            }
-           
-            InitializeComponent();
 
-            TextReader reder = File.OpenText(myTempFileInfo);
-            richTextBox1.Text = reder.ReadToEnd();
+                else if (parts[3] == "PILOT")
 
-            List<string[]> rows = File.ReadAllLines(myTempFileParsed).Select(x => x.Split(',')).ToList();
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Callsign");
-            dt.Columns.Add("Name");
-            rows.ForEach(x => {
-                dt.Rows.Add(x);
-            });
-            dataGridView1.DataSource = dt;
-        }
+                {
+                  if (parts[2] == "DRONE")
+                  {
+                    drone_count += 1;
+                  }
+                  else
+                  {
+                    pilot_count += 1;
+                  }
+                }
+              }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-            string myTempFileInfo = Path.Combine(Path.GetTempPath(), "pe_info.txt");
-            string myTempFileParsed = Path.Combine(Path.GetTempPath(), "pe_parsed.txt");
-            TextReader reder = File.OpenText(myTempFileInfo);
-            richTextBox1.Text = reder.ReadToEnd();
-        }
+            } while (true);
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+            swinfo.WriteLine($"Pilots on the network: {pilot_count}");
+            swinfo.WriteLine($"Drones on the network: {drone_count}");
+          }
 
         }
+
+      }
+
+      InitializeComponent();
+
+      TextReader reder = File.OpenText(myTempFileInfo);
+      richTextBoxInfo.Text = reder.ReadToEnd();
+
+      List<string[]> rows = File.ReadAllLines(myTempFileParsed).Select(x => x.Split(',')).ToList();
+      DataTable dt = new DataTable();
+      dt.Columns.Add("Callsign");
+      dt.Columns.Add("Name");
+      dt.Columns.Add("Onilne since");
+      rows.ForEach(x =>
+      {
+        dt.Rows.Add(x);
+      });
+      dataGridViewATC.DataSource = dt;
     }
+
+    private void richTextBox1_TextChanged(object sender, EventArgs e)
+    {
+      string myTempFileInfo = Path.Combine(Path.GetTempPath(), "pe_info.txt");
+      TextReader reder = File.OpenText(myTempFileInfo);
+      richTextBoxInfo.Text = reder.ReadToEnd();
+    }
+  }
 }
